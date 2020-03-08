66467a6ac9f64ce71a2bde1c3d3040e6be4bb263
Shadi Hortamani <shadi.hortamani@gmail.com>
2020-03-08 10:52:45 -0700

    added logic to match to approved sources

---
+++ approvedSources.js
@@ -0,0 +1,14 @@
+const Sources = [
+    'NIH',
+    'ADA',
+    'Mayo Clinic',
+    'Drugs.com',
+    'MedlinePlus',
+    'Cleveland Clinic',
+    'familydoctor.org',
+    'American Heart Association',
+    'Johns Hopkins Medicine',
+    'World Health Organization',
+    'Health Canada',
+    'WebMD'
+]
